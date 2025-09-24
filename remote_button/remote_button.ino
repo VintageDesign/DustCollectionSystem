76d4ad6f531b25546f4c3dbc08fa08ac9d36454b
Riley Kopp <koppril7@gmail.com>
2025-09-23 20:38:14 -0400

    Add debounce and status light to remote

--- remote_button/remote_button.ino
+++ remote_button/remote_button.ino
@@ -6,9 +6,14 @@
 
 esp_now_peer_info_t peerInfo;
 
-constexpr int BUTTON_PIN = 8;  // TODO Figure out the right pin later
+constexpr int BUTTON_PIN = 12;
+constexpr int STATUS_PIN = 2;
+
 
 void setup() {
+  pinMode(STATUS_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+
   esp_err_t err = esp_wifi_set_mac(WIFI_IF_STA, &remote_mac_address[0]);
   if (err == ESP_OK) {
     Serial.println("Successfully changed Mac Address");
@@ -35,32 +40,55 @@ void setup() {
     return;
   }
 
-  pinMode(BUTTON_PIN, INPUT);
+  digitalWrite(STATUS_PIN, HIGH);
 }
 
 State current_state = State::OFF;
 
+int last_button_state = 0;
+int button_state = 0;
+uint32_t debounce_time = 0;
+constexpr uint32_t DEBOUNCE_DELAY = 100;
+
 void loop() {
 
-  int button_state = digitalRead(BUTTON_PIN);
+  int button_reading = digitalRead(BUTTON_PIN);
 
-  if (button_state) {
-    state_change_request request;
-    request.new_state = current_state;
 
-    if (current_state == State::ON) {
-      current_state = State::OFF;
-    } else {
-      current_state = State::ON;
-    }
+  if (button_reading != last_button_state) {
+    debounce_time = millis();
+  }
 
-    esp_err_t result = esp_now_send(base_station_mac_address, (uint8_t *)&request, sizeof(request));
+  if ((millis() - debounce_time) > DEBOUNCE_DELAY) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
 
-    if (result == ESP_OK) {
-      Serial.println("Request Sent");
-    } else {
-      Serial.print("Error Sending Request! Error: ");
-      Serial.println(esp_err_to_name(result));
+    // if the button state has changed:
+    if (button_state != button_reading) {
+      button_state = button_reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (button_state == HIGH) {
+        state_change_request request;
+        request.new_state = current_state;
+
+        if (current_state == State::ON) {
+          current_state = State::OFF;
+        } else {
+          current_state = State::ON;
+        }
+
+        esp_err_t result = esp_now_send(base_station_mac_address, (uint8_t *)&request, sizeof(request));
+
+        if (result == ESP_OK) {
+          Serial.println("Request Sent");
+        } else {
+          Serial.print("Error Sending Request! Error: ");
+          Serial.println(esp_err_to_name(result));
+        }
+      }
     }
   }
+  last_button_state = button_reading;
+
 }
